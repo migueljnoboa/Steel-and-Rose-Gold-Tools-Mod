9b36fcecda1ebea06e7bd0bff91721ee86cda055
migueljnoboa <20190327@ce.pucmm.edu.do>
2025-08-21 23:37:05 -0400

    refactor data generator initialization to create a new pack

--- src/main/java/nine/bettertools/NinesBetterToolsDataGenerator.java
+++ src/main/java/nine/bettertools/NinesBetterToolsDataGenerator.java
@@ -7,5 +7,7 @@ public class NinesBetterToolsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
 	}
 }
